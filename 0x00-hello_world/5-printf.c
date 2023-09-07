4b95daeac287673b07b11cf9a2e7158f4b7c2d62
achaxw05 <achraf.elamlaoui@outlook.fr>
2023-09-07 18:15:10 +0100

    werror

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - this is the main function
- *
- * Retuen: 0 when successfull
+ *main - this is the main function
+ *Return: 0 when successfull
  */
 
 int main(void)
