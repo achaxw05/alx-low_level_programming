1e3e16af3afc04fcec689df7afacbdda59d18449
achaxw05 <achraf.elamlaoui@outlook.fr>
2023-09-12 19:21:21 +0100

    task1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,9 @@
 #include"main.h"
 
 /**
- * print_alphabet - utilizes on the _putchar function to print the alphabet a - z
+ * print_alphabet - utilizes on the _putchar function to print
+ * the alphabet a - z
+ * Return: 0
  */
 void print_alphabet(void)
 {
