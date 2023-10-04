e1f9bb41f8c04dfbad6e50f28d46e66a52b51ca3
root <achraf.elamlaoui@outlook.fr>
2023-10-04 14:54:44 -0700

    task5

--- 0x0B-malloc_free/100-main.c
+++ 0x0B-malloc_free/100-main.c
@@ -5,8 +5,12 @@
 /**
  * main - check the code for ALX School students.
  *
+ * @ac: The number of command-line arguments.
+ * @av: An array of strings containing the command-line arguments.
+ *
  * Return: Always 0.
  */
+
 int main(int ac, char *av[])
 {
 	char *s;
